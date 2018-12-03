81b6c52c93b601265538294e2e5282aa9557a6d9
Util <chenxy379@mail2.sysu.edu.cn>
2018-12-03 10:46:53 +0800

    发送表示上传或下载的数据包

--- code/midterm_project/src/midterm_project/datagram/Datagram.java
+++ code/midterm_project/src/midterm_project/datagram/Datagram.java
@@ -3,28 +3,31 @@ package midterm_project.datagram;
 import java.io.Serializable;
 
 public class Datagram implements Serializable {
-	private int SYN;
-	private int ACK;
-	private int FIN;
-	private int seq;
-	private int ack;
+	private int ACK;	//	分组响应
+	private int FIN;	//	断开连接
+	private int seq;	//	当前分组的序号
+	private int ack;	//	期望收到的下一个分组的序号
+	private int type;	//	0表示上传, 1表示下载
+	private int port;	//	指定服务器端口号
+	private String fileName;	//	传输文件名
 	
 	public Datagram() {
-		SYN = 0;
 		ACK = 0;
 		FIN = 0;
 		seq = 0;
 		ack = 0;
+		port = 8080;
+		fileName = "";
 	}
 	
-	public int getSYN() {
-		return SYN;
+	public String getFileName() {
+		return fileName;
 	}
-	
-	public void setSYN(int SYN) {
-		this.SYN = SYN;
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
 	}
-	
+
 	public int getACK() {
 		return ACK;
 	}
@@ -55,5 +58,21 @@ public class Datagram implements Serializable {
 	
 	public void setAck(int ack) {
 		this.ack = ack;
+	}
+	
+	public int getType() {
+		return type;
+	}
+	
+	public void setType(int type) {
+		this.type = type;
+	}
+	
+	public int getPort() {
+		return port;
+	}
+	
+	public void setPort(int port) {
+		this.port = port;
 	}
 }
